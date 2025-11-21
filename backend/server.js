12235d7627697482fd732f0678c0d775960e3c97
Puja Shukla <shuklapuja2003@gmail.com>
2025-11-21 23:16:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,15 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: [
+        "https://e-commerce-foodify-website.vercel.app",
+        "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 connectDB();
 
